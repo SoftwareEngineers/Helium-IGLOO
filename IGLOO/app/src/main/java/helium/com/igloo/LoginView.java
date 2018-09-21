01a3df6160318828a91bfbb0e63e7de489957690
mjnarvaja <maryjoynarvaja2015@gmail.com>
2018-09-21 23:57:04 +0800

    updated walkthrough

---
+++ IGLOO/app/src/main/java/helium/com/igloo/LoginView.java
@@ -0,0 +1,13 @@
+package helium.com.igloo;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class LoginView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_view);
+    }
+}
