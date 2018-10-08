b194c2c566dfe9838d8593b50e73ac843b219821
Ren Dave Lecciones <rendave.lecciones3@gmail.com>
2018-10-08 17:49:05 +0800

    added archive id in LectureModel and implemented archive listeners

--- IGLOO/app/src/main/java/helium/com/igloo/LectureActivity.java
+++ IGLOO/app/src/main/java/helium/com/igloo/LectureActivity.java
@@ -44,7 +44,7 @@ import helium.com.igloo.Adapters.QuestionAdapter;
 import helium.com.igloo.Models.LectureModel;
 import helium.com.igloo.Models.QuestionModel;
 
-public class LectureActivity extends AppCompatActivity implements Session.SessionListener, PublisherKit.PublisherListener{
+public class LectureActivity extends AppCompatActivity implements Session.SessionListener, PublisherKit.PublisherListener, Session.ArchiveListener {
 
     private TextView textLectureTitle;
     private TextView textLectureDescription;
@@ -116,6 +116,7 @@ public class LectureActivity extends AppCompatActivity implements Session.Sessio
                     final String token = response.getString("token");
                     mSession = new Session.Builder(LectureActivity.this, api_key, session_id).build();
                     mSession.setSessionListener(LectureActivity.this);
+                    mSession.setArchiveListener(LectureActivity.this);
                     mSession.connect(token);
                 } catch (JSONException error) {
                     Toast.makeText(LectureActivity.this, error.getMessage(), Toast.LENGTH_SHORT).show();
@@ -212,5 +213,15 @@ public class LectureActivity extends AppCompatActivity implements Session.Sessio
         mSession.unpublish(mPublisher);
         mSession.disconnect();
         super.onStop();
+    }
+
+    @Override
+    public void onArchiveStarted(Session session, String s, String s1) {
+
+    }
+
+    @Override
+    public void onArchiveStopped(Session session, String s) {
+
     }
 }
